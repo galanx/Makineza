ea7715ee0b44cc4c45e00314bcd95f4453415862
Albert Feka <albert@coda.tech>
2020-02-27 08:26:55 +0100

    Twig setup

--- src/start.php
+++ src/start.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel;
 use Providers\EventSubscriptionProvider;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 
 $request = Request::createFromGlobals();
@@ -12,6 +14,11 @@ $routes = include __DIR__ . '/routes.php';
 $container = include __DIR__ . '/container.php';
 
 (new EventSubscriptionProvider($container['dispatcher']))->start();
+
+$loader = new FilesystemLoader(__DIR__ . '/pages');
+$twig = new Environment($loader);
+
+//echo $twig->render('test.html', ['test' => 'Albert']);
 
 $framework = $container['framework'];
 $framework = new HttpKernel\HttpCache\HttpCache(
